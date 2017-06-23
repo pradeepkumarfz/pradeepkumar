4c1070ae226d606980ce0c0ae3c3b1bdf37beb13
pradeepkumarfz <pkumat176@gmail.com>
2017-06-23 12:10:29 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class vow
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int a=5;
+        for(int i=0;i<a;i++)
+        {
+        System.out.println("Hello");   
+        }
+        }
+        
+    }
